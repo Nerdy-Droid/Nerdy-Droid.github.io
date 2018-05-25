863ce9d078c5401203a4d6a89fdd3e181ade340f
Ashley Breymaier <abreymaier@deloitte.com>
2018-05-25 14:32:13 -0400

    updates

--- bggAPI.js
+++ bggAPI.js
@@ -3,46 +3,33 @@
 const url = "https://boardgamegeek.com/xmlapi/collection/"
 
 getName('hengst2404')
-  $('#getName').on('click', function (e) {
+  $('#getID').on('click', function (e) {
     getName($('#bggID').val())
   })
-
-    
+   
   function getName (userID) {
-   $.ajax({
-      url: url + userID,
-      dataType: "xml",
-      success: function (xml) {
-         console.log(xml)
-        $('#userName').html(userID);
+    var id = userID
+    var string = url + userID     
+    var xhttp = new XMLHttpRequest();
+         xhttp.onreadystatechange = function() {
+             if (this.readyState == 4 && this.status == 200) {
+                 myFunction(this);
+             }
+         };
+         xhttp.open("GET", string, true);
+         xhttp.send();
          
-         $(xml).find('items').each(function(){
-            var total = $(this).attr('totalitems');
-           $('#total').html(total);
-         
-        $(xml).find('item').each(function(){
-            var gameName = $(this).find('name').text();
-            var image = $(this).find('thumbnail').val();
-            var gameID = $(this).find('objectid').val();
-            var gamearr = new Array();
+         function myFunction(xml) {
             
-            console.log(gameName)
-            $("#table").append("<tr><td>" + gameName + "</td><td>" + gameID + "</td></tr>");
-            //$("#picture").append('<img id="image" src="" alt="No image loaded"/><br/>');
-            //document.getElementById('image').src = 'image';
-    });
-        });
-        
-               
-    },
-
-      error: function () {
-        updateUIError()
-      }
-    })
+             var x, i, xmlDoc, txt;
+             xmlDoc = xml.responseXML;
+             txt = new Array;
+             x = xmlDoc.getElementsByTagName('item');
+             for (i = 0; i < x.length; i++) { 
+                 txt.push(parseInt(x[i].getAttribute('objectid')));
+             }
+             console.log(txt);
+         }
   }
 
-  function updateUIError () {
-    alert('There was an error retrieving BGG data :(')
-  }
 })()
