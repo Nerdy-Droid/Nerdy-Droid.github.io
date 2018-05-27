42133230c0f4f3a4df67ad5f4da3083699bb0ffe
Ashley Breymaier <abreymaier@deloitte.com>
2018-05-27 12:48:17 -0400

    updates

--- bggAPI.js
+++ bggAPI.js
@@ -28,33 +28,28 @@ getName('aebrey')
              for (i = 0; i < x.length; i++) { 
                  txt.push(parseInt(x[i].getAttribute('objectid')));
              }
-    console.log(txt);
-    getUserGames()
-         }
-  }
+
+      $.ajax({
+      type: "GET",
+      url: "bggData.json",
+      success: function(result){
+        var data = result.game_id
+        var id = new Array();
+                
+        for (i=0; i < txt.length; i++) {
+            console.log(txt[i])
+              for (j=0; j < data.length; j++) {
+                   if (txt[i] === data[j]) {
+                    id.push(data[j])
+                               
+                }
+            }
+            console.log(id)
+                 }
+                }
+        });
+      }
+    }
 
  })()
 
-function getUserGames() {
-$.ajax({
-  type: "GET",
-  url: "bggData.json",
-  success: function(result){
-    var data = result.game_id
-    var id = new Array();
-    var txt = myFunction(xml)
-         
-    for (i=0; i < txt.length; i++) {
-        console.log(txt[i])
-          for (j=0; j < data.length; j++) {
-               if (txt[i] === data[j]) {
-                id.push(data[j])
-                            
-            }
-            
-                  }
-        console.log(id)
-             }
-            }
-    });
-  }
