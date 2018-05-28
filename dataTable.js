6a8a7c5eebb82431fd1b1d7d403b9ae2572f6e5e
Ashley Breymaier <abreymaier@deloitte.com>
2018-05-27 22:14:18 -0400

    update

--- dataTable.js
+++ dataTable.js
@@ -3,7 +3,23 @@ $(document).ready(function() {
     $('#example').DataTable({
         data: games,
         columns: [
-            {title : "Name"}
+        {title : "Name"},
+        {title : "Avg Time"},
+		{title : "BGG URL"},
+		{title : "Category"},
+		{title : "Designer"},
+		{title : "Game ID"},
+		{title : "Geek Rating"},
+		{title : "Image URL"},
+        {title : "Max Players"},
+        {title : "Max Time"},
+        {title : "Mechanic"},
+        {title : "Min Players"},
+        {title : "Min Time"},
+        {title : "Names"},
+        {title : "Rank"},
+        {title : "Weight"},
+        {title : "Year"}
         ]
     });
    });
