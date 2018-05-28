bb523137762c82ab73f790993c10fafa44e89f0e
Ashley Breymaier <abreymaier@deloitte.com>
2018-05-28 17:47:15 -0400

    updates

--- dataTable.js
+++ dataTable.js
@@ -1,11 +1,7 @@
 function getGames (games) {
     $(document).ready(function() {
-    $('#user').each( function () {
-        var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-    } );
-
-    var table = $('#user').DataTable({
+    
+       $('#user').DataTable({
         data: games,
         columns: [
             {data : "names",
@@ -28,40 +24,7 @@ function getGames (games) {
             {data : "weight"}
            
         ]
+   
+});
     });
-
-    table.columns().every( function () {
-        var that = this;
- 
-        $( 'input', this.footer() ).on( 'keyup change', function () {
-            if ( that.search() !== this.value ) {
-                that
-                    .search( this.value )
-                    .draw();
-            }
-        } );
-       });
-    });
-}
-
-
-function allGames() {
-    $(document).ready(function() {
-        $('#user').DataTable({
-            "ajax": "bggData.json",
-            "columns": [
-                {"data" : "names"},
-                {"data" : "min_players"},
-                {"data" : "max_players"},
-                {"data" : "avg_time"},
-                {"data" : "year"},
-                {"data" : "geek_rating"},
-                {"data" : "mechanic"},
-                {"data" : "category"},
-                {"data" : "weight"}
-            ]
-        
-        });
-        });
-
 }
